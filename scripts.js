b9a1ec8449826e0a405a2167727e108e5f5400af
Álef Ribeiro <alef.ribeiro@wlcsolucoes.com.br>
2022-08-10 19:07:48 -0300

    Modificações button voltar pro topo da pagina

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
-const btn = document.querySelector("#btn");
+const btn = document.querySelector(".btn");
+
+window.addEventListener('load', () => {
+    btn.classList.add("invisible")
+})
+
+addEventListener('scroll', (e) =>{
+
+    if(window.scrollY === 0){
+        btn.classList.add("invisible")
+    }else{
+        btn.classList.remove("invisible")
+    }
+})
 
 btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
 });
+
+
 
 // footer
 const dataAtual = new Date();
